dc19d4571f226efb88ef47533d9efc82fd949f3a
ZygenUEU <lamdapratama573@student.esaunggul.ac.id>
2022-10-10 11:03:24 +0700

    Pertemuan 4

--- TypeData.py
+++ TypeData.py
@@ -25,3 +25,36 @@ print (type(e))
 
 z = complex ('5-9j')
 print (z)
+
+#type data list 
+f = ["a","b","c"]
+print(f)
+print(type(f))
+
+#contoh
+list_f = [1,2,3]
+print(list_f[0])
+
+#contoh tuple
+tuple_f = 1,2,3
+print(tuple_f[0])
+
+#contoh set 
+set_f = {1,2,3}
+print(set_f)
+
+#type data set
+g = {"a","b","c"}
+print(g)
+print(type(g))
+
+#type data frozenset
+h = frozenset({1,2,3})
+print(type(h))
+
+#type data boolean
+i = True
+j = False
+print(i,j)
+print(type(i))
+print(type(j))
