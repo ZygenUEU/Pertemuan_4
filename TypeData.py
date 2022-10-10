041e2f402edb9428cbaf480d46c3afc747ea5890
ZygenUEU <lamdapratama573@student.esaunggul.ac.id>
2022-10-10 10:42:45 +0700

    Pertemuan 4

---
+++ TypeData.py
@@ -0,0 +1,27 @@
+#string
+a = "hello"
+print (a)
+print (type(a))
+
+#integer
+b = 12
+print (b)
+print (type(b))
+
+#float
+c = 10.5
+print (c)
+print (type(c))
+
+#tuple
+d = 20,5
+print (d)
+print (type(d))
+
+#data complex
+e = 1j
+print (e)
+print (type(e))
+
+z = complex ('5-9j')
+print (z)
